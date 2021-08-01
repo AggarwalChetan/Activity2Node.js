f19d833c41b953d9835ec55de0f371ca43cd708a
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-08-01 08:00:47 +0530

    Multiple scenarios code done

--- wscat.js
+++ wscat.js
@@ -1,3 +1,5 @@
+let fileSystem = require('fs');
+let path = require('path');
 let readFilesObj = require('./commands/readFiles');
 let appendFilesObj = require('./commands/appendFiles');
 let linesBreaksObj = require('./commands/linesBreaks');
@@ -7,9 +9,27 @@ let inputArg = process.argv.slice(2);
 let content = "";
 switch (inputArg[0]) {
     case "-s":
-        for (let i = 1; i < inputArg.length; ++i) {
-            content += linesBreaksObj.linesBreaks(inputArg[i]);
-            content += i == inputArg.length - 1 ? "" : "\n";
+        if (inputArg[1] == "-m") {
+            for (let i = 2; i < inputArg.length; ++i) {
+                content += linesBreaksObj.linesBreaks(inputArg[i]);
+                content += i == inputArg.length - 1 ? "" : "\n";
+            }
+            fileSystem.writeFileSync("temp.txt", content);
+            content = numberFilesObj.numberFiles("temp.txt");
+            fileSystem.unlinkSync("temp.txt");
+        } else if (inputArg[1] == "-n") {
+            for (let i = 2; i < inputArg.length; ++i) {
+                content += linesBreaksObj.linesBreaks(inputArg[i]);
+                content += i == inputArg.length - 1 ? "" : "\n";
+            }
+            fileSystem.writeFileSync("temp.txt", content);
+            content = appendFilesObj.appendFiles("temp.txt");
+            fileSystem.unlinkSync("temp.txt");
+        } else {
+            for (let i = 1; i < inputArg.length; ++i) {
+                content += linesBreaksObj.linesBreaks(inputArg[i]);
+                content += i == inputArg.length - 1 ? "" : "\n";
+            }
         }
         break;
 
@@ -19,6 +39,14 @@ switch (inputArg[0]) {
                 content += appendFilesObj.appendFiles(inputArg[i]);
                 content += i == inputArg.length - 1 ? "" : "\n";
             }
+        } else if (inputArg[1] == '-s') {
+            for (let i = 2; i < inputArg.length; ++i) {
+                content += linesBreaksObj.linesBreaks(inputArg[i]);
+                content += i == inputArg.length - 1 ? "" : "\n";
+            }
+            fileSystem.writeFileSync("temp.txt", content);
+            content = appendFilesObj.appendFiles("temp.txt");
+            fileSystem.unlinkSync("temp.txt");
         } else {
             for (let i = 1; i < inputArg.length; ++i) {
                 content += appendFilesObj.appendFiles(inputArg[i]);
@@ -33,6 +61,14 @@ switch (inputArg[0]) {
                 content += numberFilesObj.numberFiles(inputArg[i]);
                 content += i == inputArg.length - 1 ? "" : "\n";
             }
+        } else if (inputArg[1] == '-s') {
+            for (let i = 2; i < inputArg.length; ++i) {
+                content += linesBreaksObj.linesBreaks(inputArg[i]);
+                content += i == inputArg.length - 1 ? "" : "\n";
+            }
+            fileSystem.writeFileSync("temp.txt", content);
+            content = numberFilesObj.numberFiles("temp.txt");
+            fileSystem.unlinkSync("temp.txt");
         } else {
             for (let i = 1; i < inputArg.length; ++i) {
                 content += numberFilesObj.numberFiles(inputArg[i]);
